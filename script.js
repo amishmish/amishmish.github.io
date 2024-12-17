eb6b2dcb6f075ea8a5a4470a7404fe70a64f2ca6
Amishi Mishra <mishmish@unc.edu>
2024-12-16 19:03:50 -0500

    cool extra function

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-function hideSkills() {
-    skills = document.getElementById("skills");
-    skills.setAttribute("style", "display: none");
+function smallall() {
+    const treasure = document.getElementById("gold");
+    for (const coin of treasure.children) { 
+        coin.style.width = '15%';
+        coin.class = "skill hiddenskill resume"
+
+        const text = document.getElementById(coin.id + "Text");
+        if (text) {
+            text.style.display = "none";
+        }
+    }
 }
 
-function pythonbig() {
-    python = document.getElementById("python");
-    box = document.getElementById("displaySkill");
-    text = document.getElementById("pythonText");
+function skillbig(elementid) {
+    const skill = document.getElementById(elementid);
+    const text = document.getElementById(elementid + "Text");
 
-    text.setAttribute("class", "show");
-    python.setAttribute("class", "skillinDisplay resume");
-    box.setAttribute("style", "display: block")
+    smallall();
+    skill.setAttribute("style", "display: inline-block")
+    text.setAttribute("style", "display: block")
 
-    box.appendChild(python);
 
-    hideSkills();
+    text.setAttribute("class", "showText");
+    skill.setAttribute("class", "skillCOPY skillinDisplay resume");
 }
